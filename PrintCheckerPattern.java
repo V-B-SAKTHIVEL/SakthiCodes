ddc2855d4ccd6fcb5f042c0542b29a9d793db68a
V-B-SAKTHIVEL <112497833+V-B-SAKTHIVEL@users.noreply.github.com>
2022-09-30 22:19:27 +0530

    Add files via upload

---
+++ PrintCheckerPattern.java
@@ -0,0 +1,11 @@
+package com.example.demo2;
+
+public class PrintCheckerPattern {
+    public static void main(String[] args)
+    {
+        System.out.println("* * * * *");
+        System.out.println(" * * * * *");
+        System.out.println("* * * * *");
+        System.out.println(" * * * * *");
+    }
+}
