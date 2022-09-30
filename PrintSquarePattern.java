0d0616a82aa145aecdb1366b11853e30b67ac763
V-B-SAKTHIVEL <112497833+V-B-SAKTHIVEL@users.noreply.github.com>
2022-09-30 22:34:11 +0530

    Add files via upload

---
+++ PrintSquarePattern.java
@@ -0,0 +1,7 @@
+package com.example.demo2;
+
+public class PrintSquarePattern {
+    public static void main(String[] args){
+        System.out.println("* * * * * \n*       * \n*       *\n*       *\n* * * * *\n");
+    }
+}
