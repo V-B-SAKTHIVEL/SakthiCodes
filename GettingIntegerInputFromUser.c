17906ce30db5df11cec5608dfbb49d801e498028
V-B-SAKTHIVEL <112497833+V-B-SAKTHIVEL@users.noreply.github.com>
2022-10-01 15:40:13 +0530

    Add files via upload

---
+++ GettingIntegerInputFromUser.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+	int integer1;
+	printf("ENTER A INTEGER: ");
+	scanf("%d",&integer1);
+	printf("THE INTEGER YOU HAVE ENTERED IS: %d",integer1);
+}
