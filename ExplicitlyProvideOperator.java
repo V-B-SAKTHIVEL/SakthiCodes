f227700c38bd20cea1d3eb0f1e509923ea7a2db1
V-B-SAKTHIVEL <112497833+V-B-SAKTHIVEL@users.noreply.github.com>
2022-10-01 13:55:00 +0530

    Add files via upload

---
+++ ExplicitlyProvideOperator.java
@@ -0,0 +1,15 @@
+package com.example.demo2;
+
+public class ExplicitlyProvideOperator
+{
+    public static void main(String[] args)
+    {
+        int number1 = 10;
+        int number2 = 20;
+        int sum;
+        sum = (31*number1) + (17*number2);
+        System.out.println("number1: " + number1);
+        System.out.println("number2: " + number2);
+        System.out.println("THE SUM IS: "+ sum);
+    }
+}
