f24558f1751edc910d037a47d9050eb0353031a9
V-B-SAKTHIVEL <112497833+V-B-SAKTHIVEL@users.noreply.github.com>
2022-10-01 11:49:33 +0530

    Add files via upload

---
+++ PrintSheepPattern.java
@@ -0,0 +1,9 @@
+package com.example.demo2;
+
+public class PrintSheepPattern
+{
+    public static void main(String[] args)
+    {
+        System.out.println("        '  ' \n         __ \n        (oo)\n   /==========//\n  /  ||  @@ ||\n *   ||-----||\n     VV     VV\n     ''     ''");
+    }
+}
