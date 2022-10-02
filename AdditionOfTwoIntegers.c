0e7ed0f4e3c8d36e6854fee54aa3fd8b233bb9ac
V-B-SAKTHIVEL <112497833+V-B-SAKTHIVEL@users.noreply.github.com>
2022-10-02 18:14:32 +0530

    Add files via upload

---
+++ AdditionOfTwoIntegers.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+	int a,b,c;
+	printf("ENTER TWO INTEGERS: ");
+	scanf("%d %d",&a,&b);
+	c = a + b;
+	printf("THE SUM OF TWO INTEGERS %d AND %d IS: %d",a,b,c);
+}
