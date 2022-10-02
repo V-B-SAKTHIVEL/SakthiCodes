f290127b6e98d993fc19afe069ac1eb5d8c237ef
V-B-SAKTHIVEL <112497833+V-B-SAKTHIVEL@users.noreply.github.com>
2022-10-02 17:54:00 +0530

    Add files via upload

---
+++ ASCIIValueProgram.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+	char character1;
+	printf("ENTER A CHARACTER: ");
+	scanf("%c",&character1);
+	printf("THE CHARACTER YOU HAVE ENTERED IS: %c\n",character1);
+	printf("THE ASCII VALUE OF THAT CHARACTER IS: %d",character1);
+}
