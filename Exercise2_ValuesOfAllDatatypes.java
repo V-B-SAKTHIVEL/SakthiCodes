28db6ccbbe6c21ea6cabfeb2f1edf635d5ac8aef
V-B-SAKTHIVEL <112497833+V-B-SAKTHIVEL@users.noreply.github.com>
2022-10-03 13:52:20 +0530

    Add files via upload

---
+++ Exercise2_ValuesOfAllDatatypes.java
@@ -0,0 +1,22 @@
+package com.example.demo2;
+
+public class Exercise2_ValuesOfAllDatatypes
+{
+    public static void main(String[] args)
+    {
+        int a = 4;
+        byte b = 127;
+        float myfloat = 3.24f;
+        double mydouble = 7.44d;
+        char mycharacter = 'a';
+        String mystring = "WELCOME TO JAVA PROGRAMMING";
+
+        System.out.println("INTEGER: "+a);
+        System.out.println("BYTE: "+b);
+        System.out.println("FLOAT: "+myfloat);
+        System.out.println("DOUBLE: "+mydouble);
+        System.out.println("CHARACTER: "+mycharacter);
+        System.out.println("STRING: "+mystring);
+
+    }
+}
