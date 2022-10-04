2a7644fa70611200988ce2ca57539952993d2819
V-B-SAKTHIVEL <112497833+V-B-SAKTHIVEL@users.noreply.github.com>
2022-10-04 20:29:11 +0530

    Delete PrintSheepPattern.java

--- PrintSheepPattern.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo2;
-
-public class PrintSheepPattern
-{
-    public static void main(String[] args)
-    {
-        System.out.println("        '  ' \n         __ \n        (oo)\n   /==========//\n  /  ||  @@ ||\n *   ||-----||\n     VV     VV\n     ''     ''");
-    }
-}
