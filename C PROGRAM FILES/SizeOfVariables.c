b2c33445c2779624866f351add115eb034b399f4
V-B-SAKTHIVEL <112497833+V-B-SAKTHIVEL@users.noreply.github.com>
2022-10-04 21:44:44 +0530

    Add files via upload

---
+++ C PROGRAM FILES/SizeOfVariables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+	int intvar;
+	float floatvar;
+	char charvar;
+	double doublevar;
+	
+	printf("THE SIZE OF intvar IS: %d\n\n", sizeof(intvar));
+	printf("THE SIZE OF floatvar IS: %d\n\n", sizeof(floatvar));
+	printf("THE SIZE OF charvar IS: %d\n\n", sizeof(charvar));
+	printf("THE SIZE OF doublevar IS: %d\n\n", sizeof(doublevar));
+}
