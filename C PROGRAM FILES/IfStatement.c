41e8cb03aee04634de573529baa3a8e0a811defa
V-B-SAKTHIVEL <112497833+V-B-SAKTHIVEL@users.noreply.github.com>
2022-10-04 22:08:30 +0530

    Add files via upload

---
+++ C PROGRAM FILES/IfStatement.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+	int i;
+	printf("ENTER THE NUMBER: ");
+	scanf("%d",&i);
+	if (i<=10)
+	{
+		printf("%d IS LESSER THAN OR EQUAL 10.",i);
+	}
+}
