a3751afcc1179aa4b11b3afc2d8f3fdf6964855d
V-B-SAKTHIVEL <112497833+V-B-SAKTHIVEL@users.noreply.github.com>
2022-10-04 20:25:59 +0530

    Delete GettingIntegerInputFromUser.c

--- GettingIntegerInputFromUser.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <conio.h>
-void main()
-{
-	int integer1;
-	printf("ENTER A INTEGER: ");
-	scanf("%d",&integer1);
-	printf("THE INTEGER YOU HAVE ENTERED IS: %d",integer1);
-}
