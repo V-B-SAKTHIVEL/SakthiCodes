aa4dcf87ed433e2e39b3f0e823bde1bae26524c7
V-B-SAKTHIVEL <112497833+V-B-SAKTHIVEL@users.noreply.github.com>
2022-10-04 20:29:25 +0530

    Delete PrintStarPattern.java

--- PrintStarPattern.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo2;
-
-public class PrintStarPattern
-{
-    public static void main(String[] args)
-    {
-        System.out.println("    *    \n* *   * *\n   * *   \n  *   *\n *     *\n");
-    }
-}
