2da2cb3b05befe3cf52d6c935827d63e8f015e77
V-B-SAKTHIVEL <112497833+V-B-SAKTHIVEL@users.noreply.github.com>
2022-10-05 14:57:41 +0530

    Add files via upload

---
+++ JAVA PROGRAM FILES/CheckPassFail1_1.java
@@ -0,0 +1,19 @@
+package com.example.demo2;
+
+public class CheckPassFail1_1
+{
+    public static void main(String[] args)
+    {
+        int mark = 60;
+        System.out.println("THE MARK IS: " + mark);
+        if(mark >= 50)
+        {
+            System.out.println("PASS");
+        }
+
+        else
+        {
+            System.out.println("FAIL");
+        }
+    }
+}
