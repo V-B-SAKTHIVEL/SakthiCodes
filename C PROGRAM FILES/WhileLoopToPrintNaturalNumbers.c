c8673db0aa55e7232e031855739b81638bc31c54
V-B-SAKTHIVEL <112497833+V-B-SAKTHIVEL@users.noreply.github.com>
2022-10-05 10:58:07 +0530

    Add files via upload

---
+++ C PROGRAM FILES/WhileLoopToPrintNaturalNumbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+	int i=1;
+	printf("THE FIRST TEN NATURAL NUMBERS FROM 1 TO 10: \n");
+	while(i<=10)
+	{
+		printf("%d\n\n",i);
+		i++;
+	}
+	
+}
