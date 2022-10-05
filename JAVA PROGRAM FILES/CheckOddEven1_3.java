18ed98e621d2359513eba7f8f0dba5cdebb85a77
V-B-SAKTHIVEL <112497833+V-B-SAKTHIVEL@users.noreply.github.com>
2022-10-05 15:10:30 +0530

    Add files via upload

---
+++ JAVA PROGRAM FILES/CheckOddEven1_3.java
@@ -0,0 +1,19 @@
+package com.example.demo2;
+
+public class CheckOddEven1_3
+{
+    public static void main(String[] args)
+    {
+        int number = 5;
+        if(number % 2 != 0)
+        {
+            System.out.println("Odd Number");
+        }
+
+        else
+        {
+            System.out.println("Even Number");
+        }
+        System.out.println("Bye!");
+    }
+}
