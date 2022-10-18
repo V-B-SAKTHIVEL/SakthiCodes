6a35050f7d5498f1afeb16dbc8ed4a7e174e3460
V-B-SAKTHIVEL <112497833+V-B-SAKTHIVEL@users.noreply.github.com>
2022-10-18 21:50:09 +0530

    Add files via upload

---
+++ C PROGRAM FILES/HalfPyramidNumbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+	int i, j , rows;
+	printf("ENTER THE NUMBER OF ROWS: ");
+	scanf("%d", &rows);
+	for(i = 1; i <= rows; i++)
+	{
+		for(j = 1; j <= i; j++)
+		{
+			printf("%d ",j);
+		}
+		printf("\n");
+	}
+}
