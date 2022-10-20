4f7a98de335d75447d22410a692667cd8f404106
V-B-SAKTHIVEL <112497833+V-B-SAKTHIVEL@users.noreply.github.com>
2022-10-20 13:18:46 +0530

    Add files via upload

---
+++ C PROGRAM FILES/InvertedHalfPyramid.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+	int i,j,rows;
+	printf("ENTER THE NUMBER OF ROWS: ");
+	scanf("%d",&rows);
+	for(i=rows;i >= 1;i--)
+	{
+		for(j=1;j <= i; j++)
+		{
+			printf("* ");
+		}
+		printf("\n");
+	}
+	
+}
