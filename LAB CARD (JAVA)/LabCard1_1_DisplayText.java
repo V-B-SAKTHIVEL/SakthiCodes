47f183b92f677836292681b4c92f7c157b41603b
V-B-SAKTHIVEL <112497833+V-B-SAKTHIVEL@users.noreply.github.com>
2022-11-29 22:29:44 +0530

    Add files via upload

---
+++ LAB CARD (JAVA)/LabCard1_1_DisplayText.java
@@ -0,0 +1,9 @@
+package com.example.codeoftheday;
+
+public class LabCard1_1_DisplayText
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world");
+    }
+}
---
+++ LAB CARD (JAVA)/LabCard1_2_DisplayDefaultValues.java
@@ -0,0 +1,20 @@
+package com.example.codeoftheday;
+
+public class LabCard1_2_DisplayDefaultValues
+{
+    public static void main(String[] args)
+    {
+        int intType = 2;
+        double doubleType = 5.4345;
+        char charType = 'a';
+        String StringType = "Hello world...";
+        boolean booleantype = true;
+
+        System.out.println("The default values of all primitive types: ");
+        System.out.println("int datatype : " + intType);
+        System.out.println("double datatype : " + doubleType);
+        System.out.println("char datatype : " + charType);
+        System.out.println("string datatype : " + doubleType);
+        System.out.println("boolean datatype : " + booleantype);
+    }
+}
